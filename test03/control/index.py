d13899f4e3b408be6357135d02935cc3d42d46ad
futoase <futoase@gmail.com>
2009-04-16 23:03:50 +0900

    update

--- test03/control/index.py
+++ test03/control/index.py
@@ -1,9 +1,16 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+
+import web
+from model import *
+
 class index(object):
     def GET(self):
         db = dbControl()
-        pass
+        threadrecord = db.catchThreadAllRecord()
+        web.header(u'Content-Type', u'text/html')
+        render = web.template.render(u'../templates')
+        return render.index(u'BBSテスト', threadrecord)
     def POST(self):
         web.seeother(u'/')
